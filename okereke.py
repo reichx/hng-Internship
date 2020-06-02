5f558d1148bbae0c9c9f7d7bf3b21272f6379018
reichx <66280338+reichx@users.noreply.github.com>
2020-06-02 22:30:29 +0100

    okereke.py

--- okereke.py
+++ okereke.py
@@ -1,7 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
+#A simple python script to return a line of text which includes variables and strings
+
+#Declaring the variables
 internfullname = "Okereke Kalu Okereke"
 internid = "04125"
 internlang = "Python"
 
+#Output text
 print ("Hello World, this is " + internfullname + " with HNGi7 ID " + internid + " using " + internlang + " for stage 2 task")
