96050c302175b2c2a79a9f347e74a901a8d59f66
reichx <66280338+reichx@users.noreply.github.com>
2020-06-02 22:21:41 +0100

    okereke.py

---
+++ okereke.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+internfullname = "Okereke Kalu Okereke"
+internid = "04125"
+internlang = "Python"
+
+print ("Hello World, this is " + internfullname + " with HNGi7 ID " + internid + " using " + internlang + " for stage 2 task")
